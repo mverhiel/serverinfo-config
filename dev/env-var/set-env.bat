97432e1e3f750baa402018bf1461b113f61d4e4f
Marc Verhiel <mverhiel@ca.ibm.com>
2023-03-30 09:21:46 -0700

    added files

---
+++ dev/env-var/set-env.bat
@@ -0,0 +1,8 @@
+@echo on
+set ACE_NODE=ACENODE01
+set ACE_HOST=127.0.0.1
+set ACE_PORT=4414
+set ACE_SERVER=IS01
+set ACE_APP=serverInfo
+set DEPLOY_ENV=dev
+set SVC_NAME=serverinfo
---
+++ sit/env-var/set-env.bat
@@ -0,0 +1,7 @@
+@echo on
+set ACE_NODE=ACENODE01
+set ACE_HOST=127.0.0.1
+set ACE_PORT=4414
+set ACE_SERVER=IS02
+set ACE_APP=serverInfo
+set SVC_NAME=serverinfo
